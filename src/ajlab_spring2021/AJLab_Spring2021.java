08a979c625293528cc3e1447d5608426f94263c8
BaderSerhan <baderserhan@gmail.com>
2021-04-09 17:06:02 +0300

    Update First App

--- src/ajlab_spring2021/AJLab_Spring2021.java
+++ src/ajlab_spring2021/AJLab_Spring2021.java
@@ -6,11 +6,8 @@
 package ajlab_spring2021;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,38 +16,24 @@ import javafx.stage.Stage;
  */
 public class AJLab_Spring2021 extends Application {
 
-    @Override
+    @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        // Create a scene and place a button in the scene
+        Scene scene = new Scene(new Button("OK"), 200, 250);
+        primaryStage.setTitle("MyJavaFX"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        Scene scene2 = new Scene(root, 300, 250);
-
-        Stage stage = new Stage();
-        stage.setTitle("Hello World! 2");
-        stage.setScene(scene2);
-        stage.show();
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
+        Stage stage = new Stage(); // Create a new stage
+        stage.setTitle("Second Stage"); // Set the stage title
+        // Set a scene with a button in the stage
+        stage.setScene(new Scene(new Button("New Stage"), 100, 100));
+        stage.show(); // Display the stage
     }
 
     /**
-     * @param args the command line arguments
+     * The main method is only needed for the IDE with limited JavaFX support.
+     * Not needed for running from the command line.
      */
     public static void main(String[] args) {
         launch(args);
